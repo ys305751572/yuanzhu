a7a6c448956fa244b639dbcea5bf2bcebd52b147
ys305751572 <305751572@qq.com>
2016-09-26 16:24:43 +0800

    1

--- src/main/java/com/smallchill/common/task/GlobalTask.java
+++ src/main/java/com/smallchill/common/task/GlobalTask.java
@@ -6,7 +6,7 @@ public class GlobalTask implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub11
+		// TODO Auto-generated method stub
 		System.out.println("任务调度执行:" + DateKit.getTime());
 	}
 
